f1303cd13ba379240ddfc85dac69ae7c31ce268c
Jake Cohen <46077226+JakeCohen0427@users.noreply.github.com>
2022-04-06 13:20:53 -0400

    Add files via upload

---
+++ conways/README.md
@@ -0,0 +1,1 @@
+This is my project replicating the simulation "Conway's game of life, right now there will not be any visuals and the pixels will just be mapped out by coordinates# Conway-s-Game-of-life
---
+++ conways/src/Conways.java
@@ -0,0 +1,21 @@
+public class Conways 
+{
+    int xArray[];
+    int yArray[];
+
+    public static void main(String[] args)
+    {
+        CellXCoordinate();
+        CellYCoordinate();
+    }
+
+    private static int CellXCoordinate() 
+    {
+        return 0;
+    }
+
+    private static int CellYCoordinate() 
+    {
+        return 0;
+    }
+}
